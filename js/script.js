de5571b277802b90e2fe1e53bb07914bb28808e7
Timur <example@gmail.com>
2022-02-09 18:09:49 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+" use strict";
+
+// const category = 'toys';
+
+// console.log(category);
+
+
+// const user = 'Timur';
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + "- object")
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(2 + 2 * 2 === 8);
+
+const isChecked = false,
+      isClose = true;
+
+console.log(isChecked || !isClose);
