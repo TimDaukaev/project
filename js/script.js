58ca2f3da49ca6b3c1f50da6ce8345dd3cce357c
Timur <example@gmail.com>
2022-02-11 18:11:13 +0500

    add js code

--- js/script.js
+++ js/script.js
@@ -2,24 +2,75 @@
 
 " use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false, 
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false, 
+// };
 
-const a = prompt('Один из последних просмотренныъ фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренныъ фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt('Один из последних просмотренныъ фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренныъ фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
 
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+// console.log(personalMovieDB);
+
+
+
+
+
+
+// // условия
+// if (4 == 9) {
+//     console.log('ok');
+// } else {
+//     console.log('no!');
+// }
+
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+
+
+
+
+
+// циклы 
+
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 50;
+// do {
+//     console.log(num);
+//     num++;
+// } 
+// while (num < 55);
+
+let num = 50;
+for (let i = 1; i < 10; i++) {
+    if (i === 6){
+        break;
+    }
+    console.log(i);
+}
